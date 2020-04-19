8623f3f7456c069df201b413e879d2fd22d0252f
unknown <xinyi.17@intl.zju.edu.cn>
2020-04-19 13:21:31 +0800

    can jump and fall

--- final/ball.sv
+++ final/ball.sv
@@ -1,42 +1,34 @@
 //-------------------------------------------------------------------------
-//    Stickman.sv teach yyq hhhhei
+//    Stickman.sv Spring 2020											 --
 //    Adapted from Ball.sv, lab8                                         --
-//    Viral Mehta                                                        --
-//    Spring 2005                                                        --
-//                                                                       --
-//    Modified by Stephen Kempf 03-01-2006                               --
-//                              03-12-2007                               --
-//    Translated by Joe Meng    07-07-2013                               --
-//    Modified by Po-Han Huang  12-08-2017                               --
-//    Spring 2018 Distribution                                           --
-//                                                                       --
-//    For use with ECE 385 Lab 8                                         --
-//    UIUC ECE Department                                                --
+//    Store the position, motion and shape of the stickman               --
+//    Tell the ColorMapper whether this pixel is a part of the stickman  --
 //-------------------------------------------------------------------------
 
 
-module  ball ( input         Clk,                // 50 MHz clock
-                             Reset,              // Active-high reset signal
-                             frame_clk,          // The clock indicating a new frame (~60Hz)
-					input [7:0]   keycode, 				 // Accept the last received key 
-               input [9:0]   DrawX, DrawY,       // Current pixel coordinates
-               output logic  is_ball             // Whether current pixel belongs to ball or background
+module  ball ( 	input       Clk,                // 50 MHz clock
+                            Reset,              // Active-high reset signal
+                            frame_clk,          // The clock indicating a new frame (~60Hz)
+				input [7:0]   keycode, 				 // Accept the last received key 
+        		input [9:0]   DrawX, DrawY,       // Current pixel coordinates
+               	output logic  is_ball             // Whether current pixel belongs to ball or background
               );
     
-    parameter [9:0] Ball_X_Center = 10'd320;  // Center position on the X axis
-    parameter [9:0] Ball_Y_Center = 10'd240;  // Center position on the Y axis
-    parameter [9:0] Ball_X_Min = 10'd0;       // Leftmost point on the X axis
-    parameter [9:0] Ball_X_Max = 10'd639;     // Rightmost point on the X axis
-    parameter [9:0] Ball_Y_Min = 10'd0;       // Topmost point on the Y axis
-    parameter [9:0] Ball_Y_Max = 10'd479;     // Bottommost point on the Y axis
-    parameter [9:0] Ball_X_Step = 10'd1;      // Step size on the X axis
-    parameter [9:0] Ball_Y_Step = 10'd1;      // Step size on the Y axis
-    parameter [9:0] Ball_Size = 10'd4;        // Ball size
+    parameter [9:0] X_Head = 10'd200;     // Head position on the X axis (640)
+    parameter [9:0] Y_Head = 10'd320;     // Head position on the Y axis (480)
+
+    parameter [9:0] Y_Min = 10'd10;         // Ceil
+    parameter [9:0] Y_Max = 10'd300;        // Floor
+    parameter [9:0] Y_Step = 10'd2;         // Jump size on the Y axis
+
+    parameter [9:0] Height = 10'd20;        // Height of the stickman
+    parameter [9:0] Width = 10'd6;          // Width of the stickman
     
-    logic [9:0] Ball_X_Pos, Ball_X_Motion, Ball_Y_Pos, Ball_Y_Motion;
-    logic [9:0] Ball_X_Pos_in, Ball_X_Motion_in, Ball_Y_Pos_in, Ball_Y_Motion_in;
+    logic [9:0] X_Pos, X_Motion, Y_Pos, Y_Motion;
+    logic [9:0] X_Pos_in, X_Motion_in, Y_Pos_in, Y_Motion_in;
     
-    //////// Do not modify the always_ff blocks. ////////
+
+
     // Detect rising edge of frame_clk
     logic frame_clk_delayed, frame_clk_rising_edge;
     always_ff @ (posedge Clk) begin
@@ -48,29 +40,28 @@ module  ball ( input         Clk,                // 50 MHz clock
     begin
         if (Reset)
         begin
-            Ball_X_Pos <= Ball_X_Center;
-            Ball_Y_Pos <= Ball_Y_Center;
-            Ball_X_Motion <= 10'd0;
-            Ball_Y_Motion <= Ball_Y_Step;
+            X_Pos <= X_Head;
+            Y_Pos <= Y_Head;
+            X_Motion <= 10'd0;
+            Y_Motion <= 10'd0;
         end
         else
         begin
-            Ball_X_Pos <= Ball_X_Pos_in;
-            Ball_Y_Pos <= Ball_Y_Pos_in;
-            Ball_X_Motion <= Ball_X_Motion_in;
-            Ball_Y_Motion <= Ball_Y_Motion_in;
+            X_Pos <= X_Pos_in;
+            Y_Pos <= Y_Pos_in;
+            X_Motion <= X_Motion_in;
+            Y_Motion <= Y_Motion_in;
         end
     end
-    //////// Do not modify the always_ff blocks. ////////
+
     
-    // You need to modify always_comb block.
     always_comb
     begin
         // By default, keep motion and position unchanged
-        Ball_X_Pos_in = Ball_X_Pos;
-        Ball_Y_Pos_in = Ball_Y_Pos;
-        Ball_X_Motion_in = Ball_X_Motion;
-        Ball_Y_Motion_in = Ball_Y_Motion;
+        X_Pos_in = X_Pos;
+        Y_Pos_in = Y_Pos;
+        X_Motion_in = X_Motion;
+        Y_Motion_in = Y_Motion;
         
         // Update position and motion only at rising edge of frame clock
         if (frame_clk_rising_edge)
@@ -79,82 +70,47 @@ module  ball ( input         Clk,                // 50 MHz clock
             //   both sides of the operator as UNSIGNED numbers.
             // e.g. Ball_Y_Pos - Ball_Size <= Ball_Y_Min 
             // If Ball_Y_Pos is 0, then Ball_Y_Pos - Ball_Size will not be -4, but rather a large positive number.
-            if( Ball_Y_Pos + Ball_Size >= Ball_Y_Max )  // Ball is at the bottom edge, BOUNCE!
-					begin
-						Ball_X_Motion_in = 10'd0; 
-						Ball_Y_Motion_in = (~(Ball_Y_Step) + 1'b1);  // 2's complement.
-					end
-            else if ( Ball_Y_Pos <= Ball_Y_Min + Ball_Size )  // Ball is at the top edge, BOUNCE!
-					begin
-					 Ball_Y_Motion_in = Ball_Y_Step;
-					 Ball_X_Motion_in = 10'd0;
-					end
-            // TODO: Add other boundary detections and handle keypress here.
-				// X boundary detection
-            else if( Ball_X_Pos + Ball_Size >= Ball_X_Max )  // Ball is at the right edge, BOUNCE!
-               begin
-					 Ball_X_Motion_in = (~(Ball_X_Step) + 1'b1);  // 2's complement.
-					 Ball_Y_Motion_in = 10'd0; 
-					end
-            else if ( Ball_X_Pos <= Ball_X_Min + Ball_Size )  // Ball is at the left edge, BOUNCE!
-               begin
-					 Ball_X_Motion_in = Ball_X_Step; 
-					 Ball_Y_Motion_in = 10'd0; 
-					end 
-				//handle keypress
-				else
-					begin
-						unique case (keycode)
-							//press "A", direction is left
-							8'h04:
-							begin
-								Ball_X_Motion_in = (~(Ball_X_Step) + 1'b1); 
-								Ball_Y_Motion_in = 10'd0; 
-							end	
-							//press "D", direction is right
-							8'h07:
-							begin
-								Ball_X_Motion_in = Ball_X_Step; 
-								Ball_Y_Motion_in = 10'd0; 
-							end
-							//press "S", direction is down
-							8'h16:
-							begin
-								Ball_X_Motion_in = 10'd0; 
-								Ball_Y_Motion_in = Ball_Y_Step; 
-							end
-							//press "W", direction is up
-							8'h1a:
-							begin
-								Ball_X_Motion_in = 10'd0; 
-								Ball_Y_Motion_in = (~(Ball_Y_Step) + 1'b1); 
-							end
+            
+            // X position does not change
+            X_Motion_in = 10'd0;
+            
+            // press SPACE, jump
+            if (keycode == 8'h2c)
+            begin
+                if (Y_Pos > Y_Min)
+                    Y_Motion_in = (~(Y_Step) + 1'b1);  // 2's complement
+                else
+                    Y_Motion_in = 10'd0;
+            end
+            // fall because of gravity
+            else
+            begin
+                if (Y_Pos < Y_Max)
+                    Y_Motion_in = 1'b1;
+                else
+                    Y_Motion_in = 10'd0;
+			end
 
-							// the key is not pressed, or is released, the key code will be 0x00 (No Event)
-							8'h00:
-							begin
-								Ball_X_Motion_in = Ball_X_Motion; 
-								Ball_Y_Motion_in = Ball_Y_Motion; 
-							end
-							default: ;
-						endcase
-					end
             // Update the ball's position with its motion
-            Ball_X_Pos_in = Ball_X_Pos + Ball_X_Motion;
-            Ball_Y_Pos_in = Ball_Y_Pos + Ball_Y_Motion;
+            X_Pos_in = X_Pos + X_Motion;
+            Y_Pos_in = Y_Pos + Y_Motion;
         end
 
     end
+
     
-    // Compute whether the pixel corresponds to ball or background
+    // Compute whether the pixel corresponds to the stickman or not
     /* Since the multiplicants are required to be signed, we have to first cast them
        from logic to int (signed by default) before they are multiplied. */
-    int DistX, DistY, Size;
-    assign DistX = DrawX - Ball_X_Pos;
-    assign DistY = DrawY - Ball_Y_Pos;
-    assign Size = Ball_Size;
+    int relativeX, relativeY;
+    assign relativeX = DrawX - X_Pos;
+    assign relativeY = DrawY - Y_Pos;
+
     always_comb begin
-        if ( ( DistX*DistX + DistY*DistY) <= (Size*Size) ) 
+
+        if ( (relativeX>=-2 && relativeX<=2  && relativeY>=0 && relativeY<=9) 
+		    || (relativeX>=-5 && relativeX<=-3 && relativeY>=10 && relativeY<=18) 
+			 || (relativeX<=5  && relativeX>=3  && relativeY>=10 && relativeY<=18) )
             is_ball = 1'b1;
         else
             is_ball = 1'b0;
